9330019f3f156952670ab53c172c72ba233aef0b
jevina13 <67421269+jevina13@users.noreply.github.com>
2023-11-20 06:37:02 +0000

    conversion of data types

---
+++ basics_01/conversionOperations.js
@@ -0,0 +1,10 @@
+let score = "33av"
+let valueNum = Number(score)
+console.log(typeof score)
+console.log(typeof valueNum)
+console.log(valueNum)  // NaN Not a Number
+//"33" -> 33
+//"23abc" -> NaN
+//For boolean:
+//true -> 1 false -> 0
+//"" -> false  "jev" -> true
