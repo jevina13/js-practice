68658d92dbb1f806c3354c2d3ccceb1ca883f848
jevina13 <67421269+jevina13@users.noreply.github.com>
2023-12-11 08:30:11 +0000

    control flow structure

---
+++ control_flow/one.js
@@ -0,0 +1,53 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){   //=== 3 times equals means strict equal
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`); //poer is out of scope
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");  //this is implicit scope. adding two statements is not a good practice.
+
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
